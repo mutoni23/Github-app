01bdedb88f1dfcd0465b80b6e42cdf782ede797e
mutoni vivine <mutoniv5@gmail.com>
2020-11-09 22:03:27 +0200

    adding search-form.component.ts

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -41,6 +41,7 @@ export class SearchFormComponent implements OnInit {
   ngOnInit() {
       this.githubUserRequest.githubUser(this.searchMe); 
       this.users = this.githubUserRequest.users;
+      
       this.userRepos.gitUserRepos(this.searchMe);
       console.log(this.userRepos);
   }
